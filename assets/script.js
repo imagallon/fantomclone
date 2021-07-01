f792719c071886f26a4b0c75c05289f08bb5813e
Israel Magallon <browncans@gmail.com>
2021-07-01 12:51:32 -0700

    sds

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,41 @@
-var baseUrl = "https://gateway.marvel.com:443/v1/public/characters?";
-var heartContainer = document.querySelector("#hearcontianer");
-var publicApi = "5676e7d9c3a3777b9fb6a77f56ea448c";
-//var searchword = "Iron man";
-
 //var submitButton = document.querySelector("#submit-button");
 //var heroInput = document.querySelector("#hero-input");
+// var mainPageEl = document.querySelector(".main-page");
+// var heroPageEl = document.querySelector(".hero-page");
+
+// mainPageEl.style.display = "block";
+// heroPageEl.style.display = "none";
+
+$(".main-page").show();
+$(".menu-dropdown").show();
+$(".hero-page").hide();
+
+
 var heroActualNames = [
     {
-        heroname: "Iron man",
-        realname: "Tony Stark"
+        heroname: "Iron Man",
+        realname: "Tony Stark",
+        herovideoId: "f_h95mEd4TI"
     },
     {
         heroname: "Captain America",
-        realname: "Steve Rodgers"
+        realname: "Steve Rodgers",
+        herovideoId: "43NWzay3W4s"
     },
     {
-        heroname: "Spider-man",
-        realname: "Tony Stark"
+        heroname: "Spider-Man",
+        realname: "Peter Parker",
+        herovideoId: "LFoz8ZJWmPs"
     },
     {
         heroname: "Thor",
-        realname: "Thor Odinson"
-    },
-    {
-        heroname: "Ant-man",
-        realname: "Scott Lang"
+        realname: "Thor Odinson",
+        herovideoId: "v7MGUNV8MxU"
     },
     {
         heroname: "Black Widow",
-        realname: "Natasha Romanoff"
+        realname: "Natasha Romanoff",
+        herovideoId: "Fp9pNPdNwjI"
     },
     {
         heroname: "Hawkeye",
@@ -36,24 +43,27 @@ var heroActualNames = [
     },
     {
         heroname: "Hulk",
-        realname: "Bruce Banner"
-    },
-    {
-        heroname: "Captain Marvel",
-        realname: "Carol Danvers"
+        realname: "Bruce Banner",
+        herovideoId: "xbqNb2PFKKA"
     },
     {
         heroname: "Black Panther",
-        realname: "T'Challa"
+        realname: "T'Challa",
+        herovideoId: "xjDjIWPwcPU"
     },
 ]
 
 
 function searchHero(searchword) {
+
+    var marvelAPI = "5676e7d9c3a3777b9fb6a77f56ea448c";
+
     if (searchword) {
         //console.log(searchword);
-        storeHero(searchword);
-        baseUrl = "https://gateway.marvel.com:443/v1/public/characters?name=" + searchword + "&apikey=5676e7d9c3a3777b9fb6a77f56ea448c";
+        var baseUrl = "https://gateway.marvel.com:443/v1/public/characters?name=" 
+                        + searchword 
+                        + "&apikey="
+                        + marvelAPI;
     }
     //console.log(baseUrl);
     fetch(baseUrl)
@@ -66,58 +76,75 @@ function searchHero(searchword) {
     .then(function(data){
         
         heroPage(data);        
-    
-        // 
+           
     })
 
 }
 
 function heroPage(data) {
-    // showing data
-    console.log(data);
-    console.log(data.data.results);
-    console.log(data.data.results[0].id);
-    console.log(data.data.results[0].name);
-    console.log(data.data.results[0].description);
-    console.log(data.data.results[0].thumbnail.path + "/standard_fantastic.jpg");
-    console.log(data.data.results[0].urls[2].type)
-    console.log(data.data.results[0].urls[2].url);
-    //console.log(data.data.results[0].thumbnail[0].path);
-    //console.log(data.data.results[0].thumbnail[0].extension);
+    
+    createHomeButton();
 
-    //var heroid = data.data.results[0].id;
     var heroname = data.data.results[0].name;
     var heroDescription = data.data.results[0].description;
     var thumbnail = data.data.results[0].thumbnail.path + "/standard_fantastic.jpg";
-    //var heroThumbnail = data.data.results[0].thumbnail[0].path + data.data.results[0].thumbnail[0].extension;
 
     var appendBlock = 
         `
         <div class="card" style="width: 300px;">
-            <div id="heroname" class="card-divider">${heroname}</div>
+            <h2 id="heroname" class="card-divider">${heroname}</h2>
             <img id="heroThumbnail" src=${thumbnail}>
             <div class="card-section">
-                <h4 id="realName"></h4>
                 <p id="heroDescription">${heroDescription}</p>
                 <div id="embeddedVideo" class="responsive-embed">
-                    <iframe width="420" height="315" 
-                        src="https://www.youtube.com/embed/PAWjr7qo6Ds" 
-                        frameborder="0" allowfullscreen>
-                    </iframe>
+                    
                 </div>
             </div>
         </div>`;
 
+    $(".hero-page").append(appendBlock);
+
     for (var i=0; i<heroActualNames.length; i++) {
         if (heroname === heroActualNames[i].heroname) {
-            $("#realName").text() = heroActualNames[i].realname;
+            var realnameBlock = `<h4 id="realName">
+                                    \n${heroActualNames[i].realname}
+                                </h4>`;
+            $("#heroname").append(realnameBlock);
+
+            var youtubeBlock = 
+            `
+            <iframe width="420" height="315" 
+                src="https://www.youtube.com/embed/${heroActualNames[i].herovideoId}" 
+                frameborder="0" allowfullscreen>
+            </iframe>`
+
+            $("#embeddedVideo").append(youtubeBlock);
         }
     }
 
-    $("#recommended-hero").append(appendBlock);
+    //heroPageEl.style.display = "block";
+    $(".hero-page").show();
+}
+
+function createHomeButton() {
+
+    var homeButtonBlock = 
+        `
+        <button type="button" class="button secondary" id="home-button">HOME</button>
+        `;
+
+    $("#not-main-page").append(homeButtonBlock);
 
 }
 
+function goToHome() {
+    
+    console.log("1");
+//     $(this).parent().hide();
+    $(".hero-page").parent().hide();
+    $(".main-page").show();
+
+}
 
 //searchHero(searchword);
 $("#submit-button").on("click", function (event) {
@@ -126,37 +153,49 @@ $("#submit-button").on("click", function (event) {
     var searchword = $("#hero-input").val().trim();
 
     searchHero(searchword);
+
+    //mainPageEl.style.display = "none";
+    $(".main-page").hide();
 })
 
+$("#hero-input").on("submit", function (event) {
+    event.preventDefault();
 
+    var searchword = $("#hero-input").val().trim();
 
-// var heart = document.querySelector('#heart'); 
+    searchHero(searchword);
 
-// heart.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     storeHero(searchword);
-//     addToHeartedList(searchword);
-// })
+    //mainPageEl.style.display = "none";
+    $(".main-page").hide();
+})
+// var homeBtnEl = document.querySelector("#home-button")
+// homeBtnEl.addEventListener("click", goToHome());
 
+$("#home-button").on("click", "button", function (event) {
+    event.preventDefault();
+    console.log("1");
 
+    goToHome();
+    // $(".main-page").hide();
+    // $(".hero-page").show();
+    // mainPageEl.style.display = "block";
+    // heroPageEl.style.display = "none";
+})
 
-// function addToHeartedList(){
-//     var heartedHero = document.createElement('<a>');
-
-//     heartedHero.classList.add("button expanded", "btn");
-
-//     heartedHero.setAttribute("data-hero", searchword);
-//     heartedHero.textContent = searchword;
-//     heartContainer.appendChild(searchword)
-
-// } 
-
-
-    var heartedList = []
-
-
-    var heartedHeroes = JSON.parse(localStorage.getItem("heartedList")) || [];
-    localStorage.setItem("heartedHeroes", JSON.stringify(searchword));
-    heartedHeroes.push(searchword);
-    localStorage.setItem("searchword", JSON.stringify(heartedHeroes));
-
+var heart = document.querySelector('#heart'); 
+heart.addEventListener('click', function(event) {
+    event.preventDefault();
+    storeHero(searchword);
+    addToHeartedList(searchword);
+})
+function addToHeartedList(){
+    var heartedHero = document.createElement('<a>');
+    heartedHero.classList.add("button expanded", "btn");
+    heartedHero.setAttribute("data-hero", searchword);
+    heartedHero.textContent = searchword;
+    heartContainer.appendChild(searchword)
+} 
+function storeHero (searchword) {
+    var heartedHeroes = JSON.parse(localStorage.getItem("heartedHeroes", []))
+    localStorage.setItem("heartedHeroes", JSON.stringify(searchword))
+}
